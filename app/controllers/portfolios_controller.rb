77d930671ce5bc4b06c399f9323e35e901a0d4ac
lea jasmine <leajasmine@protonmail.com>
2020-10-05 09:53:22 -0400

    added edit + new buttons for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -13,11 +13,30 @@ class PortfoliosController < ApplicationController
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to @portfolios_path, notice: 'New portfolio item was added' }
+        format.html { redirect_to portfolios_path, notice: 'Record successfully updated.' }
       else
         format.html { render :new }
       end
     end
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Record was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def show
   end
 
 end
