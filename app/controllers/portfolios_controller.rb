c23b657ccb597d88578951991f5d05204292b846
lea jasmine <leajasmine@protonmail.com>
2020-10-05 12:23:07 -0400

    implemented destroy method for portfolios feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -40,4 +40,17 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
   end
 
+  def destroy
+    # Performs the look up
+    @portfolio_item = Portfolio.find(params[:id])
+
+    # Deletes the record
+    @portfolio_item.destroy
+
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.'}
+    end
+  end
+
 end
