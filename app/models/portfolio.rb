c99ebc053fdb1f781dd3b015bb233e9765be9837
lea jasmine <leajasmine@protonmail.com>
2020-10-14 07:56:09 -0400

    Integrated nested attributes for portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,7 @@
 class Portfolio < ApplicationRecord
   has_many :technologies
+  accepts_nested_attributes_for :technologies,
+                                reject_if: lambda { |attrs| attrs['name'].blank? }
 
   include Placeholder
 
