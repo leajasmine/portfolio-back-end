3b3658adc5a200fa8123fdc8a96dd9e68bc386cf
lea jasmine <leajasmine@protonmail.com>
2020-10-14 08:15:16 -0400

    Implemented form configuration for nested attributes -- adding technologies in portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,11 +9,13 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
   end
 
   def create
     # ensures that all the requirements are met // whitelisted set of parameters
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body,
+      technologies_attributes: [:name]))
 
     respond_to do |format|
       if @portfolio_item.save
