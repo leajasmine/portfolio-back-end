1e579eafbb78c47ec8e410fa3783045ad2709b7d
lea jasmine <leajasmine@protonmail.com>
2020-10-22 13:51:57 -0400

    refactored paras in portfolios controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -74,6 +74,6 @@ class BlogsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def blog_params
-      params.require(:blog).permit(:title, :body)
+      params.require(:blog)
     end
 end
