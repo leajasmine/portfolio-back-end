4ce14814f7126e0fa2baa91c3bf410ac1a4e0111
lea jasmine <leajasmine@protonmail.com>
2020-10-28 08:12:36 -0400

    added a _form partial for portfolio form

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -28,6 +28,7 @@ class PortfoliosController < ApplicationController
 
   def edit
     @portfolio_item = Portfolio.find(params[:id])
+    3.times { @portfolio_item.technologies.build }
   end
 
   def update
